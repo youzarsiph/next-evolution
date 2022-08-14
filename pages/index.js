c0095c735b8a8131f211d9824e2a00e638f80b45
Yousef Abu Shanab <josephyousef249@gmail.com>
2022-08-14 15:26:32 +0300

    Replaced imgs with svgs

--- pages/index.js
+++ pages/index.js
@@ -26,7 +26,6 @@ import {
   TableRow,
 } from "../components/view";
 import Link from "next/link";
-import Image from "next/image";
 
 export default class Home extends Component {
   constructor(props) {
@@ -511,7 +510,20 @@ export default class Home extends Component {
               {this.sizes.map((item) => {
                 return (
                   <Avatar key={item} sizingEnabled size={item} color="info">
-                    <Image src={"/curved-6.jpg"} layout={"fill"} alt="Image" />
+                    <figure className="absolute inset-0 mb-0 object-cover">
+                      <svg
+                        className="w-full h-full rounded-full"
+                        width="800"
+                        height="400"
+                        xmlns="http://www.w3.org/2000/svg"
+                        role="img"
+                        preserveAspectRatio="xMidYMid slice"
+                        focusable="false"
+                      >
+                        <title>Placeholder</title>
+                        <rect width="100%" height="100%" fill="#777"></rect>
+                      </svg>
+                    </figure>
                   </Avatar>
                 );
               })}
